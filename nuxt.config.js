606c9633ae752bdd64c4417b478c45e85607d26c
Jonathan Watson <jonathanw12@outlook.com>
2020-02-23 11:43:51 +0000

    Fixed typo

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,7 +63,7 @@ export default {
   pwa: {
     meta: {
       description:
-        "kwzz is a new quiz website built to help people create, share and play. Quizzes can be made quickly and easily with kwzz and shared so that anyone play them. Once a user has played a quiz, they can easily review their results and recieve feedback on the topics they did poorly on. Creators of quizzes can also review attempts to their quizzes to help them know other user's knowledge.",
+        "kwzz is a new quiz website built to help people create, share and play. Quizzes can be made quickly and easily with kwzz and shared so that anyone play them. Once a user has played a quiz, they can easily review their results and receive feedback on the topics they did poorly on. Creators of quizzes can also review attempts to their quizzes to help them know other user's knowledge.",
       ogHost: 'https://kwzz.xyz',
       twitterCard: 'summary',
       twitterSite: 'https://kwzz.xyz',
